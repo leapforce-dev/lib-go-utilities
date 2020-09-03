756bd24ddc040eba86160b9b99739da92cec19a9
caselongo <caselongo@gmail.com>
2020-09-03 12:31:20 +0200

    x

--- utilities.go
+++ utilities.go
@@ -5,17 +5,32 @@ import (
 	"strings"
 )
 
-// GetJsonTaggedFieldNames returns comma separated string of
+// GetTaggedFieldNames returns comma separated string of
 // fieldnames of struct having a specified tag
 //
-func GetJsonTaggedFieldNames(tag string, model interface{}) string {
+func GetTaggedFieldNames(tag string, model interface{}) string {
+	return getTaggedNames(tag, model, "field")
+}
+
+// GetTaggedTagNames returns comma separated string of
+// fieldnames of struct having a specified tag
+//
+func GetTaggedTagNames(tag string, model interface{}) string {
+	return getTaggedNames(tag, model, "tag")
+}
+
+func getTaggedNames(tag string, model interface{}, fieldOrTag string) string {
 	val := reflect.ValueOf(model)
 	list := ""
 	for i := 0; i < val.Type().NumField(); i++ {
 		field := val.Type().Field(i)
-		tag := field.Tag.Get(tag)
-		if tag != "" {
-			list += "," + field.Name
+		value, ok := field.Tag.Lookup(tag)
+		if ok {
+			if fieldOrTag == "field" {
+				list += "," + field.Name
+			} else if fieldOrTag == "tag" {
+				list += "," + value
+			}
 		}
 	}
 
