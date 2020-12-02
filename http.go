10e8b749b684b32f92cea5e7e0d13d66278df599
caselongo <caselongo@gmail.com>
2020-12-02 15:40:31 +0100

    x

--- http.go
+++ http.go
@@ -10,7 +10,7 @@ import (
 
 // DoWithRetry executes http.Request and retries in case of 500 range status code
 //
-func DoWithRetry(client *http.Client, request *http.Request, maxAttempts int, sleepSeconds int32) (*http.Response, *errortools.Error) {
+func DoWithRetry(client *http.Client, request *http.Request, maxRetries uint, secondsBetweenRetries uint32) (*http.Response, *errortools.Error) {
 	if client == nil || request == nil {
 		return nil, nil
 	}
@@ -18,14 +18,14 @@ func DoWithRetry(client *http.Client, request *http.Request, maxAttempts int, sl
 	e := new(errortools.Error)
 	e.SetRequest(request)
 
-	attempt := 1
-	for attempt <= maxAttempts {
+	attempt := uint(1)
+	for attempt <= maxRetries+1 {
 		response, err := client.Do(request)
 
 		if response.StatusCode/100 == 5 { // retry in case of status 500 range (server error)
 			attempt++
 			fmt.Printf("Starting attempt %v for %s\n", attempt, request.URL.String())
-			time.Sleep(time.Duration(sleepSeconds) * time.Second)
+			time.Sleep(time.Duration(secondsBetweenRetries) * time.Second)
 		} else {
 			if err == nil && (response.StatusCode/100 == 4 || response.StatusCode/100 == 5) {
 				err = fmt.Errorf("Server returned statuscode %v", response.StatusCode)
