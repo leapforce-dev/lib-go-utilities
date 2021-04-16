8ad4c1fdc1c4b75e393c81d240257e36c761256c
caselongo <caselongo@gmail.com>
2021-04-16 11:29:39 +0200

    x

--- arguments.go
+++ arguments.go
@@ -3,31 +3,49 @@ package utilities
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	errortools "github.com/leapforce-libraries/go_errortools"
 )
 
-func GetArguments(required *int, arguments ...*string) *errortools.Error {
+func GetArguments(required *int, arguments ...*string) (*map[string]string, *errortools.Error) {
 	argsWithoutProg := os.Args[1:]
+	var args []string = []string{}
+	var prefixedArgs map[string]string = make(map[string]string)
+
+	// first extract arguments passed with - prefix
+	for _, arg := range argsWithoutProg {
+		if strings.HasPrefix(arg, "-") {
+			if len(arg) == 1 {
+				return nil, errortools.ErrorMessage("Invalid argument '-'")
+			}
+			if !IsLetter(arg[1:2]) {
+				return nil, errortools.ErrorMessage(fmt.Sprintf("Invalid prefix '%s'", arg[:2]))
+			}
+			prefixedArgs[arg[1:2]] = arg[2:]
+		} else {
+			args = append(args, arg)
+		}
+	}
 
 	_required := len(arguments)
 	if required != nil {
 		if *required > len(arguments) {
-			return errortools.ErrorMessage(fmt.Sprintf("%v arguemnts passed but required %v", len(arguments), *required))
+			return nil, errortools.ErrorMessage(fmt.Sprintf("%v arguments passed but required %v", len(arguments), *required))
 		}
 		_required = *required
 	}
 
-	if len(argsWithoutProg) < _required {
-		return errortools.ErrorMessage("Too little arguments passed.")
+	if len(args) < _required {
+		return nil, errortools.ErrorMessage("Too little arguments passed.")
 	}
 
-	for index, arg := range argsWithoutProg {
+	for index, arg := range args {
 		if index >= _required {
 			break
 		}
 		*(arguments[index]) = arg
 	}
 
-	return nil
+	return &prefixedArgs, nil
 }
--- strings.go
+++ strings.go
@@ -1,6 +1,6 @@
 package utilities
 
-// contains checks if a string is present in a slice
+// StringSliceContains checks whether a string is present in a slice
 func StringSliceContains(s []string, str string) bool {
 	for _, v := range s {
 		if v == str {
@@ -9,4 +9,14 @@ func StringSliceContains(s []string, str string) bool {
 	}
 
 	return false
+}
+
+// IsLetter checks whether string contains of just letters
+func IsLetter(s string) bool {
+	for _, r := range s {
+		if (r < 'a' || r > 'z') && (r < 'A' || r > 'Z') {
+			return false
+		}
+	}
+	return true
 }
