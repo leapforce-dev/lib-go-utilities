ad995e7967ece3a155e77dff6e6152719ff7d55d
caselongo <caselongo@gmail.com>
2021-10-11 16:52:22 +0200

    added ParseFloat

--- conversions.go
+++ conversions.go
@@ -2,6 +2,9 @@ package utilities
 
 import (
 	"fmt"
+	"math"
+	"strconv"
+	"strings"
 	"time"
 
 	"cloud.google.com/go/civil"
@@ -69,4 +72,37 @@ func MonthEndDate(date civil.Date) civil.Date {
 	t, _ := time.Parse("2006-01-02", fmt.Sprintf("%04d-%02d-01", year, month))
 
 	return civil.DateOf(t).AddDays(-1)
+}
+
+func ParseFloat(str string) (float64, error) {
+	val, err := strconv.ParseFloat(str, 64)
+	if err == nil {
+		return val, nil
+	}
+
+	//Some number may be seperated by comma, for example, 23,120,123, so remove the comma firstly
+	str = strings.Replace(str, ",", "", -1)
+
+	//Some number is specifed in scientific notation
+	pos := strings.IndexAny(str, "eE")
+	if pos < 0 {
+		return strconv.ParseFloat(str, 64)
+	}
+
+	var baseVal float64
+	var expVal int64
+
+	baseStr := str[0:pos]
+	baseVal, err = strconv.ParseFloat(baseStr, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	expStr := str[(pos + 1):]
+	expVal, err = strconv.ParseInt(expStr, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return baseVal * math.Pow10(int(expVal)), nil
 }
